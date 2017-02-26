f19f80aa53ef09727e3d2ad17c19abfa52dbefe3
CuriousPhysicist <andrewhampson6@gmail.com>
2017-02-26 20:33:37 +0000

    Filters applied to Search index view and working.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,8 +5,102 @@ class DocumentsController < ApplicationController
       ## If a search has been carried out this collates the results 
       @documents_result = Document.search(params[:search]).order("created_at DESC")
 
+      if params[:doc_type]
+
+        ## If a doc_type is selected then filter for this type and apply subsequent filters to the sub-set
+
+        @documents_type = @documents_result.search(params[:doc_type])
+        @documents_result = @documents_type ## sets @documents_result to doc_type subset for status filtering
+
+        ## build sub-sets based on status filters
+        if params[:status1]
+          @documents_status1 = @documents_result.search(params[:status1])
+        end
+        if params[:status2]
+          @documents_status2 = @documents_result.search(params[:status2])
+        end
+        if params[:status3]
+          @documents_status3 = @documents_result.search(params[:status3])
+        end
+        if params[:status4]
+          @documents_status4 = @documents_result.search(params[:status4])
+        end
+        if params[:status5]
+          @documents_status5 = @documents_result.search(params[:status5])
+        end
+
+        ## re-initialise the instance variable with an empty ActiveRecords relation 'hash'
+        ## The if statement prevents re-initialisation if no status filters applied
+        if params[:status1] || params[:status2] || params[:status3] || params[:status4] || params[:status5]
+          @documents_result = Document.none
+        end
+
+        ## Creates a union of all subsets not nil
+        
+        ## case with doc type and status filters
+        if @documents_status1
+          @documents_result = @documents_result.or(@documents_status1)
+        end
+        if @documents_status2
+          @documents_result = @documents_result.or(@documents_status2)
+        end
+        if @documents_status3
+          @documents_result = @documents_result.or(@documents_status3)
+        end
+        if @documents_status4
+          @documents_result = @documents_result.or(@documents_status4)
+        end
+        if @documents_status5
+          @documents_result = @documents_result.or(@documents_status5)
+        end
+        
+      else 
+
+        ## case with no doc_type filter
+
+        ## build sub-sets based on status filters
+        if params[:status1]
+          @documents_status1 = @documents_result.search(params[:status1])
+        end
+        if params[:status2]
+          @documents_status2 = @documents_result.search(params[:status2])
+        end
+        if params[:status3]
+          @documents_status3 = @documents_result.search(params[:status3])
+        end
+        if params[:status4]
+          @documents_status4 = @documents_result.search(params[:status4])
+        end
+        if params[:status5]
+          @documents_status5 = @documents_result.search(params[:status5])
+        end
+
+        ## re-initialise the instance variable with an empty ActiveRecords relation 'hash'
+        ## the if statement prevents re-initialisation if no filters applied
+        if params[:status1] || params[:status2] || params[:status3] || params[:status4] || params[:status5]
+          @documents_result = Document.none
+        end
+
+        ## Creates a union of all subsets not nil
+        if @documents_status1
+          @documents_result = @documents_result.or(@documents_status1)
+        end
+        if @documents_status2
+          @documents_result = @documents_result.or(@documents_status2)
+        end
+        if @documents_status3
+          @documents_result = @documents_result.or(@documents_status3)
+        end
+        if @documents_status4
+          @documents_result = @documents_result.or(@documents_status4)
+        end
+        if @documents_status5
+          @documents_result = @documents_result.or(@documents_status5)
+        end
+      end
+
     else
-      ## SQL query to provide all documents to the view, ordered by doc_type
+      ## No search initiated ... SQL query to provide all documents to the view, ordered by doc_type
       @documents = Document.all.order(:doc_type)
       
       if params[:doc_type]
@@ -57,7 +151,7 @@ class DocumentsController < ApplicationController
         if @documents_status5
           @documents = @documents.or(@documents_status5)
         end
-        
+
       else 
 
         ## case with no doc_type filter
